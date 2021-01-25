80265db1ff8e402779457f16ec2dff801554f0ed
davidcassel123 <72877510+davidcassel123@users.noreply.github.com>
2021-01-25 12:45:00 -0500

    Update flappy-monster.js

--- flappy-monster.js
+++ flappy-monster.js
@@ -160,6 +160,22 @@ FlappyMonster.prototype.drawWalls = function() {
 game.removeExtraWalls();
 };
 
+FlappyMonster.prototype.removeExtraWalls = function() {
+  // Base
+  var game = this;
+
+  // Draw Walls
+  var walls = game.wallFactory.walls;
+
+  for(var i = 0; i < walls.length; i++){
+    if(walls[i].x + walls[i].w < 0){
+      // remove
+      walls.shift();
+    }
+  }
+};
+
+
 
 FlappyMonster.prototype.animateBackground = function() {
   // Base
